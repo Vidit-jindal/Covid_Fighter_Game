5852e90bab41b821ba994b5362fa0fe9e544b65d
Vidit Jindal <59535947+Vidit-jindal@users.noreply.github.com>
2021-01-08 22:52:03 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,2 +1,12 @@
 function load_images(){
     //player,virus,gem
+    
+    enemy_image = new Image;
+    enemy_image.src="enemy.jpeg";
+    
+    player_image= new Image;
+    player_image.src="player.jpeg";
+    
+    gem_image= new Image;
+    gem_image.src="gem.jpeg";
+}
